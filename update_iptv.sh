6f2a073b2f2e997c4596e6b091d68b2bdbd98a29
root <h0rcrux_9@protonmail.com>
2021-09-28 23:26:38 +0100

    1234

--- update_iptv.sh
+++ update_iptv.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 url="https://www.dailyiptvlist.com/portugal-iptv-smarters-free-m3u-lists-";
-day=$(date | cut -f2 -d ' ');
-month=$(date | cut -f3 -d ' ');
-year=$(date | cut -f4 -d ' ');
+day=$(date | cut -f3 -d ' ');
+month=$(date | cut -f2 -d ' ');
+year=$(date | cut -f6 -d ' ');
 
 final_url="${url}${day}-${month}-${year}";
-
+echo $final_url;
 x="";
 url_iptv=$(curl $final_url | grep "https://dailyiptvlist.com/dl/pt-" | grep -v '"');
 iptv_urls=${url_iptv/\<pre\>/"$x"};
 
 mkdir -p IPTV_List
-
+#while IFS= read -r line ; do echo "$line"; done <<< "$iptv_urls";
 while IFS= read -r line ; do wget -q -N $line -P IPTV_List/; done <<< "$iptv_urls";
 
 cat IPTV_List/* > IPTV_List/IPTV_ALL.m3u ; 
