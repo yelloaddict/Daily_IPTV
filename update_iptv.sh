52b05342ed736916f3666947a6bb063d3dab32b2
root <h0rcrux_9@protonmail.com>
2021-09-28 23:56:15 +0100

    Tue Sep 28 23:56:15 WEST 2021

--- update_iptv.sh
+++ update_iptv.sh
@@ -11,13 +11,13 @@ x="";
 url_iptv=$(curl $final_url | grep "https://dailyiptvlist.com/dl/pt-" | grep -v '"');
 iptv_urls=${url_iptv/\<pre\>/"$x"};
 
-mkdir -p IPTV_List
+#mkdir -p IPTV_List
 #while IFS= read -r line ; do echo "$line"; done <<< "$iptv_urls";
-while IFS= read -r line ; do wget -q -N $line -P IPTV_List/; done <<< "$iptv_urls";
+while IFS= read -r line ; do wget -q -N $line -P /temp/Daily_IPTV/IPTV_List/; done <<< "$iptv_urls";
 
-cat IPTV_List/* > IPTV_List/IPTV_ALL.m3u ; 
+cat IPTV_List/* > /temp/Daily_IPTV/IPTV_List/IPTV_ALL.m3u ; 
 
-rm IPTV_List/pt* 
+rm /temp/Daily_IPTV/IPTV_List/pt* 
 
 git add --all;
 git commit -m "$(date)";
