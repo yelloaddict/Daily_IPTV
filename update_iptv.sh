9e73ca9253c8f82cc702da5b85b97999855581a3
root <h0rcrux_9@protonmail.com>
2021-09-29 17:57:12 +0100

    Wed Sep 29 17:57:12 WEST 2021

--- update_iptv.sh
+++ update_iptv.sh
@@ -10,7 +10,8 @@ echo $final_url;
 x="";
 url_iptv=$(curl $final_url | grep "https://dailyiptvlist.com/dl/pt-" | grep -v '"');
 iptv_urls=${url_iptv/\<pre\>/"$x"};
-
+echo "cenas";
+echo $iptv_urls;
 #mkdir -p IPTV_List
 #while IFS= read -r line ; do echo "$line"; done <<< "$iptv_urls";
 while IFS= read -r line ; do wget -q -N $line -P IPTV_List/ ; done <<< "$iptv_urls";
